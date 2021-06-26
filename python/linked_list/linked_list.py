864f0b2dbff65edcd4bba1c1fe0dd1e5872ba9ad
Glenn Elliron Clark <Elliron.Clark@gmail.com>
2021-06-26 07:19:07 -0700

    first test passing

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,57 +1,103 @@
 class Node:
-    """
-    Class creates an Linked-List
-    The object inside has two parameters, itself as the first parameter and the head as the second parameter
+    # Function to initialize node object
+    def __init__(self, value, next = None):
+        self.value = value # assigns an address and data to the node
+        self.next = next # sets address of next node to none
 
-
-    """
-    # Creates Nodes
-    def _init_(self, value, next = None):
-        self.value = value
-        self.next = next
-
+# Class LinkedList is taking individual Node instances and linking them together in a list
 class LinkedList:
-    """
-    Class creates an empty Linked List
-    Define an __init__ method with the parameter head
-    Define a method that inserts a new node with a new value into the head of the list.
-    Define a method called includes, that uses a boolean to determine if a value exists as a nodes value in the linked list.
-    Define a method called __str__ which takes in no arguments and returns a string of all the values in the linked list.
-    The string should be in the form "{ a } -> { b } -> { c } -> NONE"
+    # function to initialize head
+    def __init__(self):
+        self.head = None
 
-Any exceptions or errors that come from your code should be semantic, capturable errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
-Be sure to follow your language/frameworks standard naming conventions (e.g. C# uses PascalCasing for all method and class names).
-    """
-    # Creates Linked Lists
-    def __init__(self, head = None):
-        self.head = head
+        # function to print list from head to end
+    # def to_string(self):
+    #     temp = self.head
+    #     while (temp):
+    #         print (temp.data)
+    #         temp = temp.next
 
-    # Inserts a new node with a new value at the head or start of the linked list.  The value being added cannot be None.
     def insert(self, value):
-        node = Node(value)
 
-        if self.head is not None:
-            node.next = self.head
-        self.head = node
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+        return self
 
-    # Checks to see if a value exists in the linked list, returns a boolean
-    # def includes(self, value):
-    #     if self.value is not None:
-    #         if self.value in
+# https://www.geeksforgeeks.org/search-an-element-in-a-linked-list-iterative-and-recursive/
+    def includes(self, search_key):
+        current = self.head
+        while current != None:
+            if current.data == search_key:
+                return True
 
-current = ll2.head
+            current = current.next
 
-while current.value is not None:
-    current = current.next
+        return False
 
-if __name__ == "__main__":
-    ll2 = LinkedList()
-    ll2.insert('apples')
+    def __str__(self):
+        string = ""
+        current = self.head
+        while current is None:
+            string += f"{ {current.value} } ->"
+            current = current.next
 
-    ll2.insert('orange')
+        string += f" None "
 
-    ll2.insert(' pears')
+        return string
 
-    ll1 = LinkedList(Node('apples', Node(('orange', Node('pear')))))
+        # function to append to the end of the list
+    # def append_to_end(self, new_data):
+    #     # Create new node, insert data, set next as none
+    #     new_node = Node(new_data)
+    #     # if empty make new node head of list
+    #     if self.head is None:
+    #         self.head = new_node
+    #         return
+    #     # else traverse till the last node in list
+    #     last = self.head
+    #     while (last.next):
+    #         last = last.next
+    #     # change the next of last node
+    #     last.next = new_node
 
-    ll1.next = None
+    #     # insert a new node after a specified node
+    # def insert_new_node_after(self, previous_node, new_data):
+    #     # chest if previous node exists
+    #     if previous_node is None:
+    #         print("node does not exist")
+    #         return
+    #     # create new node and put in data
+    #     new_node = Node(new_data)
+    #     # change next of new Node to next of previous_node
+    #     new_node.next = previous_node.next
+    #     # change next of the previous_node to new_node
+    #     previous_node.next = new_node
+
+        # insert a new node before a specified node
+    # def insert_new_node_before(self, next_node, new_data):
+    #     # check if next_node exists
+    #     if next_node is None:
+    #         print("node does not exist")
+    #         return
+    #     # create new node and put in data
+    #     new_node = Node(new_data)
+    #     # find the node whose next is = to next_node and replace it with the new nodes new_data
+    #     if self.head.new_data is next_node:
+    #         self.head.next = new_data
+    #     # change next of new node to next_node
+    #     new_node.next = next_node
+
+                # code execution
+if __name__ =='__main__':
+
+    # Starts with the empty list
+    llist = LinkedList()
+    llist.insert("apples")
+    entire_list = str(llist)
+    print(entire_list)
+    # llist.insert_new_node_before(llist.)
+    # llist.insert_new_node_after(8, 4)
+    print
+
+
