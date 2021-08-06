b852bfebdc45526c0dba00b3852ad020700b71fa
Glenn Elliron Clark <Elliron.Clark@gmail.com>
2021-08-06 14:05:16 -0700

    added tree max

---
+++ python/code_challenges/tree_max/README.md
@@ -0,0 +1,11 @@
+# Challenge Summary
+Find the max value of a Binary Tree
+
+## Whiteboard Process
+![challenge-16-whitboard](code_challenges/img/Challenge 16 Whiteboard.png)
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
---
+++ python/code_challenges/tree_max/tree_max.py
@@ -0,0 +1,20 @@
+from trees.tree import *
+from stack_queue.stack_and_queue import Queue
+
+def function(BinaryTree):
+    queue = Queue()
+    max = BinaryTree.root.value.head.value[0]
+    queue.enqueue(BinaryTree.root)
+    while queue.peek():
+        front = queue.dequeue()
+        if front.left:
+            queue.enqueue(front.left)
+        if front.right:
+            queue.enqueue(front.right)
+        current = front.value.head
+        while current is not None:
+            for number in current.value:
+                if number > max:
+                    max = number
+            current = current.next
+    return max
