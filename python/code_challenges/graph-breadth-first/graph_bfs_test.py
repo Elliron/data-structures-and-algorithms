d6c52594eb28586e8eb611aae1d93974b6eabe4f
Glenn Elliron Clark <Elliron.Clark@gmail.com>
2021-09-22 20:35:35 -0700

    still working on full implementation and testing

---
+++ python/code_challenges/graph-breadth-first/graph_bfs_test.py
@@ -0,0 +1,30 @@
+from graph_breadth_first import *
+
+
+def test_vertex():
+    node = Vertex("apple")
+    assert node.value == "apple"
+
+def test_vertex_not():
+    node = Vertex("apple")
+    assert node.value != "apple pie"
+
+def test_edge():
+    edgie = Edge("apple", "banana")
+    assert edgie.vertex == "apple"
+
+def test_edge_not():
+    edgie = Edge("apple", "banana")
+    assert edgie.vertex != "apple pie"
+
+def test_graph():
+    g = Graph()
+    g.add_node("apple")
+    g.add_node("pear")
+    assert g.size() == 2
+
+def test_graph_not():
+    g = Graph()
+    g.add_node("apple")
+    g.add_node("pear")
+    assert g.size() != 3
